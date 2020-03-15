57ba37d6b47260f13d4d0d26ce2a94571f8db4fd
ChironEvans <55260739+ChironEvans@users.noreply.github.com>
2020-03-16 09:21:01 +1300

    Initial Commit

---
+++ src/weekFour/Main4.java
@@ -0,0 +1,16 @@
+package weekFour;
+
+public class Main4 {
+
+	public static void main(String[] args) {
+		int[] numbers = {11,22,33};
+		System.out.println(numbers[1]);
+		for (int n : numbers) {
+			System.out.println(n);
+		}
+		for (int i = 0; i < numbers.length; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
