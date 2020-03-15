ee3e26519c4680bcfef84fcb87a9100415f32f4c
ChironEvans <55260739+ChironEvans@users.noreply.github.com>
2020-03-16 10:59:37 +1300

    Cleaned up increments

--- src/reverseArray/Reverser.java
+++ src/reverseArray/Reverser.java
@@ -4,13 +4,12 @@ public class Reverser {
 	protected int[] revArray;
 	protected int lower;
 	protected int upper;
-	protected int revI;
 	
 	public int[] reverseArray(int[] anArray) {
-		revArray = new int[anArray.length];
-		revArray = anArray;
-		this.revI = this.revArray.length - 1;
-		for (int i = 0; i < this.revI; i++) {
+		this.revArray = new int[anArray.length];
+		this.revArray = anArray;
+		int revI = this.revArray.length - 1;
+		for (int i = 0; i < revI; revI --, i++) {
 			
 			// Get the upper and lower values from the current scope
 			this.lower = this.revArray[i];
@@ -19,9 +18,6 @@ public class Reverser {
 			// swap the values in the array
 			this.revArray[i] = this.upper;
 			this.revArray[revI] = this.lower;
-			
-			// increment the upper limit down
-			this.revI --;
 		}
 		
 		return revArray;
